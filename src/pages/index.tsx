4f295a1705ba612f6ae489359575e3a2c9d75d4c
Matheus Bruno Almeida <manancialsup@hotmail.com>
2022-08-24 18:20:05 -0300

    header

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Heading } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header />
+
+      
+    </>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+    darkGreen: '#1C4A47'
+}
+
+export const theme = extendTheme({ colors })
