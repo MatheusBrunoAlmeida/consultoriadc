9fbbadf9a09a2be031ef3589f646b740333931e7
Matheus Bruno Almeida <manancialsup@hotmail.com>
2022-08-30 12:16:02 -0300

    arrumando erro para o build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document{
                 <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 {/* @ts-ignore */}
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&family=Montserrat:wght@400;500;600;700;800&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
